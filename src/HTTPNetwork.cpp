0300618b7e2492c9f1b268e4e57a36b790f7b883
LazyPanda07 <semengricenko@gmail.com>
2021-04-04 16:22:02 +0500

    Added HTTPNetwork

---
+++ src/HTTPNetwork.cpp
@@ -0,0 +1,101 @@
+#include "HTTPNetwork.h"
+
+#include <algorithm>
+#include <charconv>
+
+using namespace std;
+
+namespace web
+{
+	constexpr uint16_t averageHTTPRequestSize = 1500;
+	const string contentLengthHeader = "Content-Length";
+	const string crlfcrlf = "\r\n\r\n";
+
+	HTTPNetwork::HTTPNetwork(SOCKET clientSocket) :
+		Network(clientSocket)
+	{
+
+	}
+
+	HTTPNetwork::HTTPNetwork(const string& ip, const string& port) :
+		Network(ip, port)
+	{
+
+	}
+
+	int HTTPNetwork::sendData(const vector<char>& data)
+	{
+		try
+		{
+			return Network::sendBytes(data.data(), data.size());
+		}
+		catch (const WebException&)
+		{
+			return -1;
+		}
+	}
+
+	int HTTPNetwork::receiveData(vector<char>& data)
+	{
+		data.resize(averageHTTPRequestSize);
+		int totalSize = 0;
+		int lastPacket = 0;
+		int bodySize = -1;
+		bool isFindEnd = false;
+
+		while (!isFindEnd)
+		{
+			if (totalSize >= data.size() - 100)
+			{
+				data.resize(data.size() * 2);
+			}
+
+			lastPacket = recv(Network::clientSocket, data.data() + totalSize, data.size() - totalSize, NULL);
+
+			if (lastPacket == SOCKET_ERROR || !lastPacket)
+			{
+				throw WebException();
+			}
+
+			totalSize += lastPacket;
+
+			string_view findHeader(data.data(), totalSize);
+			string_view::const_iterator contentLength = search
+			(
+				findHeader.begin(), findHeader.end(), contentLengthHeader.begin(), contentLengthHeader.end(),
+				[](const char& first, const char& second)
+				{
+					return tolower(first) == tolower(second);
+				}
+			);
+
+			if (contentLength == findHeader.end())
+			{
+				isFindEnd = findHeader.find(crlfcrlf) != string_view::npos;
+			}
+			else
+			{
+				size_t endOfHTTP = findHeader.find(crlfcrlf);
+
+				if (bodySize == -1)
+				{
+					size_t contentLengthHeaderPosition = distance(findHeader.begin(), contentLength) + contentLengthHeader.size() + 2;
+					string_view contentLengthValue = findHeader.substr(contentLengthHeaderPosition, findHeader.find("\r\n", distance(findHeader.begin(), contentLength)) - contentLengthHeaderPosition);
+
+					from_chars(contentLengthValue.data(), contentLengthValue.data() + contentLengthValue.size(), bodySize);
+
+					data.resize(data.size() + bodySize);
+				}
+
+				if (endOfHTTP != string_view::npos)
+				{
+					isFindEnd = findHeader.size() == (endOfHTTP + crlfcrlf.size() + bodySize);
+				}
+			}
+		}
+
+		data.resize(totalSize);
+
+		return totalSize;
+	}
+}
--- src/HTTPNetwork.h
+++ src/HTTPNetwork.h
@@ -1,1 +1,47 @@
 #pragma once
+
+#include "BaseNetwork.h"
+
+#pragma comment (lib, "HTTP.lib")
+#pragma comment (lib, "JSON.lib")
+#pragma comment (lib, "SocketStreams.lib")
+
+namespace web
+{
+	/// <summary>
+	/// Network functions for HTTP
+	/// </summary>
+	class HTTPNetwork : public web::Network
+	{
+	public:
+		/// <summary>
+		/// Server side constructor
+		/// </summary>
+		/// <param name="clientSocket">socket from WSA accept function</param>
+		HTTPNetwork(SOCKET clientSocket);
+
+		/// <summary>
+		/// Client side constructor
+		/// </summary>
+		/// <param name="ip">server address</param>
+		/// <param name="port">server listen socket port</param>
+		HTTPNetwork(const std::string& ip, const std::string& port);
+
+		/// <summary>
+		/// Default send function
+		/// </summary>
+		/// <param name="data">that sends through HTTP</param>
+		/// <returns>total send bytes</returns>
+		int sendData(const std::vector<char>& data) override;
+
+		/// <summary>
+		/// Specific HTTP receive data function
+		/// </summary>
+		/// <param name="data">output data from HTTP</param>
+		/// <returns>total receive bytes</returns>
+		/// <exception cref="web::WebException"></exception>
+		int receiveData(std::vector<char>& data) override;
+
+		~HTTPNetwork() = default;
+	};
+}
